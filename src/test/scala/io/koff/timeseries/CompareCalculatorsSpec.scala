d52431f671d8e1e48b85cf09a2595a5f5582f4a1
Aleksei Shamenev <coffius@gmail.com>
2017-02-12 14:42:25 +0300

    spec with comparison of outputs of different algorithms

---
+++ src/test/scala/io/koff/timeseries/CompareCalculatorsSpec.scala
@@ -0,0 +1,28 @@
+package io.koff.timeseries
+
+import java.io.File
+
+import io.koff.timeseries.naive.NaiveCalculator
+import io.koff.timeseries.optimized.OptimizedCalculator
+import org.scalatest.{FreeSpec, Matchers}
+
+import scala.collection.mutable
+
+class CompareCalculatorsSpec extends FreeSpec with Matchers {
+  private val file = new File("data/data_scala.txt")
+  private val bufferSize = 100
+  private val rollingWindow = 60
+
+  "check outputs of both implementations" in {
+    val optimizedAccum = mutable.ArrayBuilder.make[String]()
+    OptimizedCalculator.calculate(file, bufferSize, rollingWindow, optimizedAccum += _.toPrintString)
+
+    val naiveAccum = mutable.ArrayBuilder.make[String]()
+    NaiveCalculator.calculate(file, bufferSize, rollingWindow, naiveAccum += _.toPrintString)
+
+    val optimized = optimizedAccum.result().toSeq
+    val naive = naiveAccum.result().toSeq
+
+    naive should contain theSameElementsInOrderAs optimized
+  }
+}
