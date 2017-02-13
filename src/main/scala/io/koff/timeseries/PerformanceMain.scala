59dac30847f55af34932cbafdb913571d2ec9222
Aleksei Shamenev <coffius@gmail.com>
2017-02-13 11:17:15 +0300

    a special main class for performance tests

---
+++ src/main/scala/io/koff/timeseries/PerformanceMain.scala
@@ -0,0 +1,29 @@
+package io.koff.timeseries
+
+import java.io.File
+
+import io.koff.timeseries.common.Output
+import io.koff.timeseries.optimized.OptimizedCalculator
+
+/**
+  * The main class of the application
+  */
+object PerformanceMain {
+  private val BufferSize = 1000
+  private val RollingWindow = 60
+  def main(args: Array[String]): Unit = {
+    val inputFile = if(args.length > 0) {
+      val file = new File(args(0))
+      if(!file.exists() || !file.isFile) {
+        throw new IllegalArgumentException(s"Can't find a file with the name: ${file.getAbsolutePath}")
+      } else {
+        file
+      }
+    } else {
+      throw new IllegalArgumentException("Please provide a filename for analysis")
+    }
+
+    println("calculation has been started")
+    OptimizedCalculator.calculate(inputFile, BufferSize, RollingWindow, identity[Output])
+  }
+}
